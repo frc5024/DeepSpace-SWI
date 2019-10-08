cda73b0822cd472e5e053730092ae7174d5687cb
Carter Tomlenovich <cartertom@tuta.io>
2019-10-08 17:04:49 -0400

    More code added

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -1,63 +1,82 @@
-package frc.robot.subsystems;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.Solenoid;
 import frc.lib5k.loops.loopables.LoopableSubsystem;
 import frc.lib5k.utils.RobotLogger;
+import frc.robot.Constants;
 
-/**
- * The intake includes the slider, piston, finger, and all related sensors
- */
 public class Intake extends LoopableSubsystem {
     RobotLogger logger = RobotLogger.getInstance();
-    public static Intake m_instance = null;
 
+    // Creating components
+
+    DoubleSolenoid m_finger;
+    Solenoid m_piston;
+    WPI_TalonSRX m_slider;  
+
+    // Variables
+    boolean isFingerLowered = false;
+    boolean isPistionExtended = false;
+    double sliderSpeed = 0;
+    
     public Intake() {
+        // Constructing Finger
+        m_finger = new DoubleSolenoid(Constants.PCM.finger_forward,Constants.PCM.finger_reverse);
+        logger.log("[Intake] Constructing Double Solenoid");
 
-        // Set the subsystem name for logging
-        name = "Intake";
+        // Constructing Soleniod
+        m_piston = new Solenoid(Constants.PCM.piston);
+        logger.log("[Intake] Constructing Solenoid");
+              
+        // Constructing Talon
+        m_slider = new WPI_TalonSRX(Constants.slider_id);
+        logger.log("[Intake] Constructing Talon");
     }
 
     /**
-     * Get the current Intake instance
-     * 
-     * @return Intake instance
+     * @param lowered Should the finger be lowered
      */
-    public static Intake getInstance() {
-        if (m_instance == null) {
-            m_instance = new Intake();
-        }
-
-        return m_instance;
+    public void setFingerLowered(boolean lowered) {
+        this.isFingerLowered = lowered;
     }
 
-    public void setFingerLowered(boolean lowered){
+    /**
+     * @param extended Should the piston be extended
+     */
+    public void setPistonExtended(boolean extended) {
+
+        this.isPistionExtended = extended;
+
     }
 
-    public void setPistonExtended(boolean extended){
+    /**
+     * @param speed Sets the speed of the slider
+     */
+    public void setSliderSpeed(double speed) {
 
-    }
+        this.sliderSpeed = speed;
 
-    public void setSliderSpeed(double speed){
-        
-    }
+    } 
 
-    @Override
+
+     @Override
     public void periodicOutput() {
 
     }
 
-    public void periodicInput() {
-    }
+    @Override
+    public void periodicInput() {}
 
     @Override
-    public void outputTelemetry() {
-    }
+    public void outputTelemetry() {}
 
     @Override
-    public void stop() {
-    }
+    public void stop() {}
 
     @Override
-    public void reset() {
-    }
+    public void reset() {}
+
+    
 
 }
