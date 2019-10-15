e10a45582a873b4c1b89af8b5764bf046a6a4882
rsninja <jnick722547@gmail.com>
2019-10-15 16:24:07 -0400

    subsystem done

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -2,6 +2,8 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.DigitalInput;
 import frc.lib5k.loops.loopables.LoopableSubsystem;
 import frc.lib5k.utils.RobotLogger;
 import frc.robot.Constants;
@@ -15,10 +17,18 @@ public class Intake extends LoopableSubsystem {
     Solenoid m_piston;
     WPI_TalonSRX m_slider;  
 
+    // Creating sensors
+    DigitalInput m_leftHall;
+    DigitalInput m_centreHall;
+    DigitalInput m_rightHall;
+    
     // Variables
     boolean isFingerLowered = false;
-    boolean isPistionExtended = false;
+    boolean isPistonExtended = false;
     double sliderSpeed = 0;
+    
+    boolean left,right,centre = false;
+    int side = 0;
     
     public Intake() {
         // Constructing Finger
@@ -32,50 +42,127 @@ public class Intake extends LoopableSubsystem {
         // Constructing Talon
         m_slider = new WPI_TalonSRX(Constants.slider_id);
         logger.log("[Intake] Constructing Talon");
-    }
 
+        // Construction sensors
+        m_leftHall = new DigitalInput(Constants.DIO.slider_left_limit);
+        m_centreHall = new DigitalInput(Constants.DIO.slider_centre_limit);
+        m_rightHall = new DigitalInput(Constants.DIO.slider_right_limit);
+        logger.log("[Intake] Constructing Sensors");
+
+        // Set the subsystem name for logging
+        name = "Intake";
+    }
+                            
     /**
      * @param lowered Should the finger be lowered
      */
     public void setFingerLowered(boolean lowered) {
-        this.isFingerLowered = lowered;
+        isFingerLowered = lowered;
     }
 
     /**
      * @param extended Should the piston be extended
      */
     public void setPistonExtended(boolean extended) {
-
-        this.isPistionExtended = extended;
-
+        isPistonExtended = extended;
     }
 
     /**
      * @param speed Sets the speed of the slider
      */
     public void setSliderSpeed(double speed) {
+        double s = speed;
 
-        this.sliderSpeed = speed;
+        if (left) {
+            if (s < 0) {
+                s = 0;
+            }
+        }
+        if (right) {
+            if (s > 0) {
+                s = 0;
+            }                            
+        }
 
+        sliderSpeed = s;
     } 
 
+    // when called, the slider speed will be set to the value needed to center the slider
+    public void centerSlider() {
+        double speed = 0;
+
+        if (!centre) {
+            if (side == 1) {
+                speed = -1;
+            }
+            if (side == -1) {
+                speed = 1;
+            }
+        } else {
+            side = 0;
+        }
+
+        sliderSpeed = speed;
+    } 
 
      @Override
     public void periodicOutput() {
 
+        // toggle the finger
+        if (isFingerLowered) {
+            m_finger.set(DoubleSolenoid.Value.kForward);
+        } else {
+            m_finger.set(DoubleSolenoid.Value.kReverse);
+        } 
+        
+        // toggle the piston
+        if (isPistonExtended) {
+            m_piston.set(true);
+        } else {
+            m_piston.set(false);
+        }
+
+        // set slider speed
+        m_slider.set(sliderSpeed);
     }
 
     @Override
-    public void periodicInput() {}
+    public void periodicInput() {
+        left = !m_leftHall.get();
+        centre = !m_centreHall.get();
+        right = !m_rightHall.get();
+
+        if(this.centre) {
+            if (m_slider.get() > 0) {
+                side = 1;
+            }
+            if (m_slider.get() < 0 ) {
+                side = -1;
+            }
+        }
+    }
 
     @Override
-    public void outputTelemetry() {}
+    public void outputTelemetry() {
+        SmartDashboard.putBoolean("[Intake] is finger extended", isFingerLowered);
+        SmartDashboard.putBoolean("[Intake] is piston extended", isPistonExtended);
+        SmartDashboard.putNumber("[Intake] slider speed", sliderSpeed);
+        SmartDashboard.putBoolean("[Intake] Right Hall Sensor", right);
+        SmartDashboard.putBoolean("[Intake] Centre Hall Sensor", centre);
+        SmartDashboard.putBoolean("[Intake] Left Hall Sensor", left);
+    }
 
     @Override
-    public void stop() {}
+    public void stop() {
+        sliderSpeed = 0.0;
+        isFingerLowered = false;
+        isPistonExtended = false;  
+    }
 
     @Override
-    public void reset() {}
+    public void reset() {
+        this.stop();
+    }
 
     
 
